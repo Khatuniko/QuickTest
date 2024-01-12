d0779ef7084759289ba5d6f22b85913b42fa0ee8
Khatuna Chaphodze <111879031+Khatuniko@users.noreply.github.com>
2024-01-13 01:17:47 +0400

    add pdf files in regulation page with download this page done

--- src/app/module/main-pg/regulation/regulation.component.ts
+++ src/app/module/main-pg/regulation/regulation.component.ts
@@ -6,5 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./regulation.component.scss']
 })
 export class RegulationComponent {
+  downloadPdf37() {
+    const pdfUrl = 'assets/reglament/new37.pdf';
+    const link = document.createElement('a');
+    link.href = pdfUrl;
+    link.download = 'new-37.pdf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
 
+}
+downloadPdf511() {
+  const pdfUrl = 'assets/reglament/new511.pdf';
+  const link = document.createElement('a');
+  link.href = pdfUrl;
+  link.download = 'new-511.pdf';
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+
+}
+downloadPdf522() {
+  const pdfUrl = 'assets/reglament/new522.pdf';
+  const link = document.createElement('a');
+  link.href = pdfUrl;
+  link.download = 'new-522.pdf';
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+
+}
+downloadPdf510() {
+  const pdfUrl = 'assets/reglament/new510.pdf';
+  const link = document.createElement('a');
+  link.href = pdfUrl;
+  link.download = 'new-510.pdf';
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+
+}
 }
