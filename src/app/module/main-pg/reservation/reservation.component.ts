fd5ec208118718361ebd82cac96b0828b0322075
Khatuna Chaphodze <111879031+Khatuniko@users.noreply.github.com>
2024-02-20 20:36:16 +0400

    add validators in form

--- src/app/module/main-pg/reservation/reservation.component.ts
+++ src/app/module/main-pg/reservation/reservation.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-reservation',
@@ -7,23 +7,64 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./reservation.component.scss']
 })
 export class ReservationComponent implements OnInit {
-  
-  myForm: FormGroup = new FormGroup({
-    'carname': new FormControl('', [Validators.required]),
-    'number': new FormControl('', [Validators.required]),
-    'date': new FormControl('', [Validators.required]),
-    'selectedOption': new FormControl('', [Validators.required]),
-  });
 
-  
+  registerForm:any = FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder:FormBuilder){
+
+  }
+
+  onSubmit(){
+    this.submitted = true;
+
+    if(this.registerForm.invalid){
+      return
+    }
+    alert("Success")
+
+  }
+
   ngOnInit() {
+    //validations
+
+    this.registerForm = this.formBuilder.group({
+      carNumber:['', Validators.required],
+      mobileNumber:['', Validators.required,Validators.minLength(9)],
+      date:['', Validators.required],
+      branches: ['', Validators.required],
+      checkbox:['', Validators.required]
+    })
 
   }
 
-  btnbooking(){
-    alert("ვიზიტი წარმატებით დაიჯავშნა");
 
+
+  
+  // myForm: FormGroup = new FormGroup({
+  //   // registerForm: FormGroup = new FormGroup({
+  //   'firstName': new FormControl('', [Validators.required]),
+  //   'lastName': new FormControl('', [Validators.required]),
+  //   'date': new FormControl('', [Validators.required]),
+  //   'selectedOption': new FormControl('', [Validators.required]),
+  // });
+
+  
+ 
+
+  // sxva 
+
+
+
+  
+  onDateChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const selectedDate = target.value;
+    target.classList.add('selected-date');
   }
+  
+
+
 
 
 }
