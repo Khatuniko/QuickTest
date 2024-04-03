38b0a052754547c64372273fbc5d6c9a756049e2
Khatuna Chaphodze <111879031+Khatuniko@users.noreply.github.com>
2024-04-03 20:54:57 +0400

    clean comment and mistakes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -8,12 +7,6 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class AppComponent{
   title = 'QuickTestProject';
-
-  // constructor(public translate: TranslateService) {
-  //   translate.addLangs(['geo', 'en']);
-  //   translate.setDefaultLang('geo');
-  //   const browerLang = translate.getBrowserLang();
-  // }
 
 }
 
--- src/app/module/main-pg/main-pg.component.ts
+++ src/app/module/main-pg/main-pg.component.ts
@@ -6,21 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./main-pg.component.scss']
 })
 export class MainPgComponent {
-  timeSlots: string[] = [];
-  selectedHour: string | null = null;
 
-
-  constructor() {
-    // Generate time slots from 10:00 to 19:00 in half-hour intervals
-    for (let hour = 10; hour <= 19; hour++) {
-      this.timeSlots.push(`${hour}:00`);
-      this.timeSlots.push(`${hour}:30`);
-    }
-  }
-
-  handleHourSelection(event: Event) {
-    const selectedValue = (event.target as HTMLSelectElement).value;
-    console.log(`Selected meeting hour: ${selectedValue}`);
-    this.selectedHour = selectedValue; // Set the selected meeting hour
-  }
 }
